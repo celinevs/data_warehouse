2fbae8a118f8080fe62013365e05ecdaf298897c
cia2003 <graciansamosir@gmail.com>
2025-11-14 20:39:59 +0700

    adding flask

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from os import environ
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URL'] = environ.get('DATABASE_URL')
+
+db = SQLAlchemy(app)
