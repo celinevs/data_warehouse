d7bda32765858471bb5e377d52b684f5cb351d01
cia2003 <graciansamosir@gmail.com>
2025-11-17 12:43:52 +0700

    add cashier dimension

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.extensions import db, cors
 from app.routes.date_routes import date_bp
 from app.routes.payment_method_routes import payment_method_bp
+from app.routes.cashier_routes import cashier_bp
 from os import environ
 
 def create_app():
@@ -17,7 +18,7 @@ def create_app():
     # register blueprints
     app.register_blueprint(date_bp, url_prefix="/api/flask/dates")
     app.register_blueprint(payment_method_bp, url_prefix="/api/flask/payment-methods")
-    app.register_blueprint(payment_method_bp, url_prefix="/api/flask/cashiers")
+    app.register_blueprint(cashier_bp, url_prefix="/api/flask/cashiers")
 
     # create tables
     with app.app_context():
--- backend/app/routes/cashier_routes.py
+++ backend/app/routes/cashier_routes.py
@@ -2,28 +2,28 @@ from flask import Blueprint, request, jsonify
 from app.models.cashier_model import CashierData
 from app.extensions import db
 
-payment_method_bp = Blueprint("cashiers", __name__)
+cashier_bp = Blueprint("cashiers", __name__)
 
-@payment_method_bp.route('/', methods=['GET'])
-def get_payment_methods():
-    payment_methods = CashierData.query.all()
+@cashier_bp.route('/', methods=['GET'])
+def get_cashiers():
+    cashiers = CashierData.query.all()
     return jsonify({
         "message": "OK",
-        "count": len(payment_methods),
-        "data": [d.json() for d in payment_methods]
+        "count": len(cashiers),
+        "data": [d.json() for d in cashiers]
     }), 200
 
-@payment_method_bp.route('/', methods=['POST'])
-def create_payment_method():
+@cashier_bp.route('/', methods=['POST'])
+def create_cashier():
     data = request.get_json()
     cashier = CashierData(**data)
     db.session.add(cashier)
     db.session.commit()
     return jsonify({"message": "Created", "data": cashier.json()}), 201
 
-@payment_method_bp.route('/id/<string:id>', methods=['GET'])
-def get_payment_method_by_sk(id):
-    payment_method = CashierData.query.get(id)
-    if not payment_method:
+@cashier_bp.route('/id/<string:id>', methods=['GET'])
+def get_cashier_by_sk(id):
+    cashier = CashierData.query.get(id)
+    if not cashier:
         return jsonify({"message": "Not Found"}), 404
-    return jsonify({"message": "OK", "data": payment_method.json()}), 200
+    return jsonify({"message": "OK", "data": cashier.json()}), 200
