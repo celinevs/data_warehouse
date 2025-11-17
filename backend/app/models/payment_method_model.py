9ef8bf8c85e9436daae4ed1232bcd34462f9645a
cia2003 <graciansamosir@gmail.com>
2025-11-17 10:32:44 +0700

    fill the payment's dimension

--- backend/app/models/payment_method_model.py
+++ backend/app/models/payment_method_model.py
@@ -2,9 +2,29 @@ from app.extensions import db
 
 
 class DateData(db.Model):
-    __tablename__ = "dim_tanggal"
+    __tablename__ = "dim_metode_pembayaran"
 
-    # Primary Key (format: YYYYMMDD)
+    # Surrogate Key (Primary)
+    metode_pembayaran_sk = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    # Natural Key
+    metode_pembayaran_id = db.Column(db.String(50), unique=True, nullable=False)
+
+    # Descriptive Attributes
+    nama_metode_pembayaran = db.Column(db.String(100), nullable=False)
+    jenis_pembayaran = db.Column(db.String(50), nullable=False)
+    penyedia = db.Column(db.String(50), nullable=True)
+    apakah_digital = db.Column(db.Boolean, nullable=False)
+
+    def json(self):
+        return {
+            "metode_pembayaran_sk": self.metode_pembayaran_sk,
+            "metode_pembayaran_id": self.metode_pembayaran_id,
+            "nama_metode_pembayaran": self.nama_metode_pembayaran,
+            "jenis_pembayaran": self.jenis_pembayaran,
+            "penyedia": self.penyedia,
+            "apakah_digital": self.apakah_digital
+        }
 
     def json(self):
         return {
