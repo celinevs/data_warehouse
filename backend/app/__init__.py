2b0f2c19fc38804a1c39197cf4c57d7632bd7df4
cia2003 <graciansamosir@gmail.com>
2025-11-19 20:24:35 +0700

    add product dimension

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,7 @@ from app.routes.date_routes import date_bp
 from app.routes.payment_method_routes import payment_method_bp
 from app.routes.cashier_routes import cashier_bp
 from app.routes.promotion_routes import promotion_bp
+from app.routes.product_routes import product_bp
 from os import environ
 
 def create_app():
@@ -21,6 +22,7 @@ def create_app():
     app.register_blueprint(payment_method_bp, url_prefix="/api/flask/payment-methods")
     app.register_blueprint(cashier_bp, url_prefix="/api/flask/cashiers")
     app.register_blueprint(promotion_bp, url_prefix="/api/flask/promotions")
+    app.register_blueprint(product_bp, url_prefix="/api/flask/promotions")
 
     # create tables
     with app.app_context():
